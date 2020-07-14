b60f42d46e7be056613af1389a0d261e74915db1
Sibel <sibelclarus@gmail.com>
2020-07-14 14:32:05 -0400

    py file added

---
+++ 002-milliseconds-converter/MillsecondConverter.py
@@ -0,0 +1,34 @@
+from flask import Flask , render_template ,request
+
+app = Flask(__name__) #arayuz
+
+def convert(milliseconds):
+    hour_ms = 60*60*1000
+    hours = milliseconds // hour_ms
+    ms_left = milliseconds % hour_ms
+    minutes_ms = 60*1000
+    minutes = ms_left // minutes_ms
+    ms_left = ms_left % minutes_ms
+    seconds = ms_left // 1000 
+    return f"{hours} hour/s " *(hours != 0) + f"{minutes} minute/s " *(minutes != 0) + f"{seconds} second/s " *(seconds != 0) or f"just {milliseconds} millisecond/s " *(milliseconds < 1000) 
+
+@app.route('/', methods = ['GET']) #anasayfa  # get degerleri aliyoruz,#post hafizaya alma,goruntulemiyor.yazdirma
+def main_get():
+    return render_template ('index.html', developer_name = 'Aysun',not_valid = False) 
+
+@app.route('/', methods = ['POST'])
+def main_post():
+    alpha = request.form ["number"]
+    if not alpha.isdigit():
+        return render_template ('index.html', developer_name = 'Aysun',not_valid = True) 
+    
+    number =int(alpha)
+    if (0 > number):
+        return render_template ('index.html', developer_name = 'Aysun',not_valid = True)
+
+    return render_template ('result.html', milliseconds=number, result= convert (number), developer_name = 'Aysun') 
+
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0', port = 80, debug = True)
+    #app.run(debug=True)  
+    
