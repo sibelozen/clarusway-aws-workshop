c9c3a716d2fbbdc5d9f4aed036e6e2e692414668
Sibel <sibelclarus@gmail.com>
2020-07-14 16:04:40 -0400

    python file renamed

--- 001-roman-numerals-converter/romanNumeralConvertor.py
+++ 001-roman-numerals-converter/romanNumeralConvertor.py
@@ -36,5 +36,5 @@ def result_get():
 
 if __name__ == '__main__':
     # app.run('localhost', port=5000, debug=True)  
-    app.run(debug=True)   
+    # app.run(debug=True)   
     app.run('0.0.0.0', port=80)
